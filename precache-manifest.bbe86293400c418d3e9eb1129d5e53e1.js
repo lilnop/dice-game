abfd2649e65b092ff74d55aae9d5185c876c8e41
lilnop <lilsnop@gmail.com>
2019-09-17 06:53:35 -0700

    Updates

---
+++ precache-manifest.bbe86293400c418d3e9eb1129d5e53e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c80e474fa8b39b6cb081c1cecf806ef",
+    "url": "/dice-game/index.html"
+  },
+  {
+    "revision": "019ed0d2e6540cfd958d",
+    "url": "/dice-game/static/css/main.27056f54.chunk.css"
+  },
+  {
+    "revision": "e113f7225a9a0fc45def",
+    "url": "/dice-game/static/js/2.64cfdff0.chunk.js"
+  },
+  {
+    "revision": "019ed0d2e6540cfd958d",
+    "url": "/dice-game/static/js/main.46f804bf.chunk.js"
+  },
+  {
+    "revision": "cd971590f08008df1143",
+    "url": "/dice-game/static/js/runtime~main.ecd78382.js"
+  }
+]);
